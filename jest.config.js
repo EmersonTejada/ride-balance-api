228cf3cb8436b51fbcee7d916573c4518fedd6ec
EmersonTejada <emerson.8965@gmail.com>
2026-03-16 16:50:28 -0400

    refactor: Update Jest configuration to support ESM with ts-jest.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 export default {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   transform: {
-    ...tsJestTransformCfg,
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
 };
