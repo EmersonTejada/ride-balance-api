34b7c8714326e7e882fe5607e98cec08035b88ef
Emerson Tejada <emerson.8965@gmail.com>
2026-04-12 18:24:41 -0400

    feat(terraform): add AWS EC2 instance configuration with user data for Docker installation

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,3 +49,49 @@ resource "aws_security_group" "ride_balance_sg" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "ride_balance_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.ride_balance_sg.id]
+
+  key_name = "ride-balance-key"
+
+  tags = {
+    Name = "RideBalance-Terraform"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bas
+              apt-get update
+              apt-get install -y ca-certificates curl
+
+              install -m 0755 -d /etc/apt/keyrings
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+              chmod a+r /etc/apt/keyrings/docker.asc
+
+              echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+              apt-get update
+              apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+              systemctl start docker
+              systemctl enable docker
+              
+              usermod -aG docker ubuntu
+              EOF
+
+}
